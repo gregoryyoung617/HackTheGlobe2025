ea19136d7ab862fa1e1a5b81fad292fb02c4c692
gregoryyoung617 <gregoryyoung617@gmail.com>
2025-03-22 23:33:58 -0400

    add wardrobe attributes

--- frontend/src/wardrobe.js
+++ frontend/src/wardrobe.js
@@ -16,6 +16,18 @@ export default function Wardrobe(props){
     const [currUrl, setCurrUrl] = useState(null);
     const [currTimesWorn, setCurrTimesWorn] = useState(null);
     const [showAdd, setShowAdd] = useState(false);
+    const [imageObjs, setImageObjs] = useState([]);
+    const [numImgs, setNumImgs] = useState(0);
+
+    const [enterName, setEnterName] = useState(null);
+    const [enterType, setEnterType] = useState(null);
+    const [enterBrand, setEnterBrand] = useState(null);
+    const [enterSize, setEnterSize] = useState(null);
+
+    const [viewName, setViewName] = useState(null);
+    const [viewType, setViewType] = useState(null);
+    const [viewBrand, setViewBrand] = useState(null);
+    const [viewSize, setViewSize] = useState(null);
 
     const handleDrop = (e) => {
         e.preventDefault();
@@ -72,6 +84,7 @@ export default function Wardrobe(props){
             // Get the download URL
             const url = await getDownloadURL(storageRef);
             setImage(URL.createObjectURL(imageFile)); // Show a preview
+            createClothing(props.db, props.user.uid, enterType, enterName, url, enterBrand, enterSize);
         } catch (error) {
             console.error("Error uploading file:", error);
             alert("Failed to upload image. Please try again.");
@@ -79,17 +92,26 @@ export default function Wardrobe(props){
             setUploading(false);
             //setImage(null);
             fetchImages();
+            setShowAdd(false);
         }
     };
 
     const fetchImages = async () => {
-        const storage = getStorage();
-        const folderRef = ref(storage, `images/${props.user.uid}/wardrobe/`);
-        const result = await listAll(folderRef);
-        const urls = await Promise.all(result.items.map((itemRef) => getDownloadURL(itemRef)));
-        setImageUrls(urls);
-        console.log(urls);
+        // const storage = getStorage();
+        // const folderRef = ref(storage, `images/${props.user.uid}/wardrobe/`);
+        // const result = await listAll(folderRef);
+        // const urls = await Promise.all(result.items.map((itemRef) => getDownloadURL(itemRef)));
+        const imgObjs = await listAllClothing(props.db, props.user.uid, "timesWorn");
+        setImageObjs(imgObjs);
+        setNumImgs(imgObjs.length);
+        
+        //setImageUrls(urls);
+        
     };
+
+    useEffect(()=>{
+        console.log(imageObjs);
+    },[imageObjs])
 
     useEffect(() => {
         fetchImages();
@@ -98,6 +120,12 @@ export default function Wardrobe(props){
     const handleImgClick = (url)=>{
         console.log(`setting url to ${url}`);
         setCurrUrl(url);
+        const currImg = imageObjs.find((obj) => obj.clothingPictureURL === url);
+        console.log(currImg);
+        setViewName(currImg.clothingName);
+        setViewBrand(currImg.company);
+        setViewSize(currImg.size);
+        setViewType(currImg.clothingType);
         setShowImg(true);
     }
 
@@ -113,18 +141,19 @@ export default function Wardrobe(props){
                 <img
                     src={addSvg}
                     onClick={handleAddImg}
-                    className="acc-svg add-svg"/>
+                    className="add-svg"/>
             </div>
+            <span>{`You have ${numImgs} ${numImgs === 1 ? "item":"items"}`}</span>
             <div className="img-grid">
-                {imageUrls && imageUrls.map((url, index) => (
+                {imageObjs && imageObjs.map((img, index) => (
                     <img
                         key={index}
-                        src={url}
+                        src={img.clothingPictureURL}
                         alt={`Image ${index}`}
                         loading="lazy" // Lazy loading attribute
                         className="img-square"
                         onClick={()=>{
-                            handleImgClick(url)
+                            handleImgClick(img.clothingPictureURL)
                         }}
                     />
                 ))}
@@ -145,6 +174,7 @@ export default function Wardrobe(props){
                                 padding: "20px",
                                 textAlign: "center",
                                 backgroundColor: dragging ? "#f0f8ff" : "#fff",
+                                marginBottom:"20px",
                             }}
                             onDrop={handleDrop}
                             onDragOver={handleDragOver}
@@ -166,6 +196,7 @@ export default function Wardrobe(props){
                                 id="fileInput"
                                 onChange={handleFileChange}
                             />
+
                             <div style={{display:'flex', gap:'1rem', justifyContent:'center'}}>
                                 <label
                                     htmlFor="fileInput"
@@ -182,27 +213,70 @@ export default function Wardrobe(props){
                                 >
                                     Choose File
                                 </label>
-
-                                <label
-                                    style={{
-                                        display: "inline-block",
-                                        marginTop: "10px",
-                                        padding: "10px 10px",
-                                        backgroundColor: "#007bff",
-                                        color: "#fff",
-                                        width:'101px',
-                                        borderRadius: "5px",
-                                        cursor: "pointer",
-                                    }}
-                                    className="green"
-                                    onClick={uploadImage}
-                                >
-                                    Upload
-                                </label>
                             </div>
                             {/* <Button
                             onClick={uploadImage}
                             className="green">Upload</Button> */}
+                        </div>
+
+                        <div style={{
+                                border: "2px dashed #ccc",
+                                borderRadius: "10px",
+                                padding: "20px",
+                                textAlign: "center",
+                                backgroundColor: dragging ? "#f0f8ff" : "#fff",
+                                display:"flex",
+                                flexDirection:"column",
+                                justifyContent:"flex-start",
+                                alignItems:"flex-start"
+                            }}>
+                            <span className="add-title">Name:</span>
+                            <input 
+                                onChange={(e)=>{
+                                    setEnterName(e.target.value)
+                                }}
+                                className="add-input"
+                            />
+
+                            <span className="add-title">Brand:</span>
+                            <input 
+                                onChange={(e)=>{
+                                    setEnterBrand(e.target.value)
+                                }}
+                                className="add-input"
+                            />
+                            <span className="add-title">Type:</span>
+                            <input 
+                                onChange={(e)=>{
+                                    setEnterType(e.target.value)
+                                }}
+                                className="add-input"
+                            />
+
+                            <span className="add-title">Size:</span>
+                            <input 
+                                onChange={(e)=>{
+                                    setEnterSize(e.target.value)
+                                }}
+                                className="add-input"
+                            />
+
+                            <label
+                                style={{
+                                    display: "inline-block",
+                                    marginTop: "30px",
+                                    padding: "10px 10px",
+                                    backgroundColor: "#007bff",
+                                    color: "#fff",
+                                    width:'101px',
+                                    borderRadius: "5px",
+                                    cursor: "pointer",
+                                }}
+                                className="green"
+                                onClick={uploadImage}
+                            >
+                                Upload
+                            </label>
                         </div>
 
                     </Card.Body>
@@ -217,11 +291,23 @@ export default function Wardrobe(props){
                     style={{padding:'0'}}>
                     <img src={currUrl}
                     className="wardrobe-img"/>
+                    <div style={{display:"flex",
+                                flexDirection:"column",
+                                justifyContent:"flex-start",
+                                alignItems:"flex-start",
+                                padding:"2rem",}}>
+                        <span>{`${viewName}`}</span>
+                        <span>{`${viewBrand}`}</span>
+                        <span>{`${viewType}`}</span>
+                        <span>{`${viewSize}`}</span>
+
+                    </div>
                 </Modal.Body>
+                
             </Modal>
-            <button onClick={() => getIndividualClothing(props.db, "youtube.com")}>Get clothing by url</button>
+            {/* <button onClick={() => getIndividualClothing(props.db, "youtube.com")}>Get clothing by url</button>
             <button onClick={() => listAllClothing(props.db, props.user.uid, "timesWorn")}>List all clothing</button>
-            <button onClick={() => createClothing(props.db, props.user.uid, "Shirt", "Kaws Tokyo's First", "youtube.com", "Uniqlo", "XXXL")}>Create</button>
+            <button onClick={() => createClothing(props.db, props.user.uid, "Shirt", "Kaws Tokyo's First", "youtube.com", "Uniqlo", "XXXL")}>Create</button> */}
         </div>
     )
 }
