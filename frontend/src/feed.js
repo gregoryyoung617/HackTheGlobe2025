c6f34a36377e582de8410561fa430b2f18e5175b
aryuyitu <117850039+aryuyitu@users.noreply.github.com>
2025-03-22 21:54:35 -0400

    api

--- frontend/src/feed.js
+++ frontend/src/feed.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { createClothing } from "./api";
 
-export default function Feed(){
+export default function Feed(props){
     return (
         <div>
             feed
+            <button onClick = {() => createClothing(props.db, "userID", "clothingType", "clothingName", "clothingPictureURL", "company", "size")}>Create sample clothing</button>
         </div>
     )
 }
